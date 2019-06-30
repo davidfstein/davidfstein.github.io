2d0e78b8a2733c3946f86dde20e8aa03c31efbde
steindak <david.stein@novartis.com>
2019-06-29 23:34:57 -0400

    added footer icons

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import './footer.css';
 
 export default function Footer() {
     return (
         <footer className='footer'>
-            Bye
+            <div className='social-icons'>
+                <a className="icon-link" href="https://github.com/davidfstein">
+                    <FontAwesomeIcon className="social-icon" icon={faGithub} />
+                </a>
+                <a className="icon-link" href='https://www.linkedin.com/in/david-stein-035ba441/'>
+                    <FontAwesomeIcon className="social-icon" icon={faLinkedin} />
+                </a>
+            </div>
         </footer>
     )
 }
