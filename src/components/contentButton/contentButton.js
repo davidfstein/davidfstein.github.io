09131d0830af6d9385e572a527aee61ee8bfda17
steindak <david.stein@novartis.com>
2019-07-02 16:31:03 -0400

    fade out chevron when scrolled past

--- src/components/contentButton/contentButton.js
+++ src/components/contentButton/contentButton.js
@@ -5,11 +5,52 @@ import './contentButton.css';
 
 class ContentButton extends React.Component {
 
+    getElementOffset = (element) => {
+        const de = document.documentElement;
+        const box = element.getBoundingClientRect();
+        const top = box.top + window.pageYOffset - de.clientTop;
+        const left = box.left + window.pageXOffset - de.clientLeft;
+        return { top: top, left: left };
+    }
+
+    hideButton = () => {
+        const button = document.querySelector('.content-chevron');
+        const top = this.getElementOffset(button).top;
+        if (window.scrollY > top - 100) {
+            button.style.opacity = 0;
+        }
+    }
+
+    showButton = () => {
+        const button = document.querySelector('.content-chevron');
+        if (window.scrollY <= 100) {
+            button.style.opacity = 1;
+        }
+    }
+
+    setScrollListeners = () => {
+        window.addEventListener('scroll', this.hideButton);
+        window.addEventListener('scroll', this.showButton);
+    }
+
+    removeScrollListeners = () => {
+        window.removeEventListener('scroll', this.hideButton);
+        window.removeEventListener('scroll', this.showButton);
+    }
+
     toContent = () => {
         const about = document.querySelector('#about');
         about.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
+
+    componentDidMount() {
+        this.setScrollListeners();
+    } 
     
+    componentWillUnmount() {
+        this.removeScrollListeners();
+    }
+
     render() {
         return (
             <div className='button-div'>
