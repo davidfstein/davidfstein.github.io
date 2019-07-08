308988e966f192e66d4296714828142832e1c384
steindak <david.stein@novartis.com>
2019-07-07 21:22:23 -0400

    toggle menu on scroll

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink, NavbarToggler, Collapse } from 'reactstrap';
 import './header.css';
 
 class Header extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapsed: true,
+            atTop: true
+        };
+    }
+
+    atTop = () => {
+        this.setState({
+            atTop: window.scrollY === 0,
+        });
+    }
+
+    toggleNavbar = () => {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
+
     removeHeaderClassAtTop = () => {
-        if (window.scrollY === 0) {
+        if (this.state.atTop) {
             const header = document.querySelector('.navbar');
             header.style.backgroundColor = '#EAE7DC';
         }
@@ -18,11 +38,13 @@ class Header extends React.Component {
 
     setScrollListeners = () => {
         window.addEventListener('scroll', this.swapHeaderClass);
+        window.addEventListener('scroll', this.atTop);
         window.addEventListener('scroll', this.removeHeaderClassAtTop);
     }
 
     removeScrollListeners = () => {
         window.removeEventListener('scroll', this.swapHeaderClass);
+        window.removeEventListener('scroll', this.atTop);
         window.removeEventListener('scroll', this.removeHeaderClassAtTop);
     }
 
@@ -37,17 +59,38 @@ class Header extends React.Component {
     render() {
         return (
             <Navbar>
-                <Nav navbar className='flex-row'>
-                    <NavItem>
-                        <NavLink href="/resume#about">About Me</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/resume#experience">Experience</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/resume">Skills</NavLink>
-                    </NavItem>
-                </Nav>
+                {   
+                    this.state.atTop
+                    ? 
+                    <Nav navbar className='flex-row'>
+                        <NavItem>
+                            <NavLink href="/resume#about">About Me</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/resume#experience">Experience</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="/resume">Skills</NavLink>
+                        </NavItem>
+                    </Nav>
+                    :
+                    <div>
+                        <NavbarToggler onClick={this.toggleNavbar} className="mr-2 navbar-dark" />
+                        <Collapse isOpen={!this.state.collapsed} navbar>
+                            <Nav navbar className='flex-row'>
+                                <NavItem>
+                                    <NavLink href="/resume#about">About Me</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="/resume#experience">Experience</NavLink>
+                                </NavItem>
+                                <NavItem>
+                                    <NavLink href="/resume">Skills</NavLink>
+                                </NavItem>
+                            </Nav>
+                        </Collapse>
+                    </div>
+                }
                 <NavbarBrand href="/resume" className="ml-auto">David Stein</NavbarBrand>
             </Navbar>
         );
