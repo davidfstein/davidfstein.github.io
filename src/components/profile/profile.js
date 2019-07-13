f0d80050f2f3d7da9fb6f835a78eec3309707c81
steindak <david.stein@novartis.com>
2019-07-12 22:17:04 -0400

    fixed bug where header overlapped title on auto-scroll

--- src/components/profile/profile.js
+++ src/components/profile/profile.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import profPic from '../../assets/photo.png';
 import './profile.css';
 
 export default function Profile() {
     return (
         <div className='container'>
-            {/* <img className="profile-pic" alt="Profile" src={profPic}/> */}
             <div id='above-the-fold'>
                 <div id='above-content'>
                     <h1 className="welcome">Welcome</h1>
-                    <p>Taxidermy typewriter man bun, shabby chic gentrify nulla la croix ad pop-up. 
-                        Helvetica narwhal humblebrag excepteur. Chia godard excepteur cred eu VHS 
-                        cardigan cornhole ipsum palo santo qui labore 90's vaporware heirloom. 
-                        Adipisicing pinterest lorem glossier lyft pour-over mollit nisi hoodie 
-                        everyday carry raw denim incididunt sriracha fingerstache. YOLO pug locavore, 
-                        fam yuccie magna palo santo ut enim cred irony next level ugh live-edge ennui. 
-                        Duis tumblr pok pok distillery, in vegan flexitarian portland semiotics butcher et.</p>
+                    <p>As a highly motivated student at Northeastern University, my interest in biomimicry 
+                        and software design drove me to pursue a Bachelors of Science degree in Computer 
+                        Science and Biology. My strengths include, responsive thinking, and fast-paced, 
+                        real-time problem solving skills. I find that my abilities in these areas are 
+                        helpful when searching for coding solutions. I aspire to use my knowledge of 
+                        CS and Biology to help develop novel biological technologies.
+                    </p>
                 </div>
             </div>
-            {/* <div id='image-placeholder'></div> */}
         </div>
     )
 }
